f44657473bd843366beed833c0f05c0bddb8248d
Lakkanna Walikar <lakkannawalikar@gmail.com>
2019-05-14 18:47:51 +0530

    published first version vs code markeplace

---
+++ README.md
@@ -0,0 +1,38 @@
+# css-converter (VS-Code extension)
+
+Convert a kebab-case css to camel case css or vice-versa.
+
+## How to use
+  1. Select the CSS text you want convert
+  2. Open  `Command Pallette (Shift + Command + P) / (Shift + Control + P)` in VS-Code and enter `CSS Converter`
+  3. Select `CSS Converter` from searched result
+  4. **How's the josh** your css is converted.
+
+## Example
+  ### **kebab-case to camelCase**
+    background-color: #FFF;   ===>   backgroundColor: "#FFF",
+
+  ### **camelCase to kebab-case**
+    backgroundColor: "#FFF",   ===>   background-color: #FFF;
+
+## Features
+
+* Convert your html css to jsStyle which support in react style elements.
+
+* Auto identifies CSS style format.
+
+## Known Issues
+
+* Need to fix issue of indentation after replacing new CSS
+* Additional double quotes if try to convert kebab-case css to camelCase.
+
+## Release Notes
+
+### 0.0.1
+**Initial release of CSS Converter** 
+> Identifies format of css and converts it to kebab-case or camelCase based on which css format we selected.
+
+-----------------------------------------------------------------------------------------------------------
+
+
+**Enjoy!**
---
+++ src/extension.ts
@@ -0,0 +1,65 @@
+import * as vscode from 'vscode';
+import * as _ from 'lodash';
+
+const generateKebabCaseCSS = (lines: string[]): string[] => {
+	let returnLines: string[] = [];
+	_.forEach(lines, (line) => {
+		if (!_.isEmpty(line)) {
+			let [key, value] = _.split(line, ":");
+			const newLine = `${_.kebabCase(_.trim(key))}: ${_.trim(value)};`;
+			returnLines.push(newLine);
+		}
+	});
+	return returnLines;
+};
+
+const generateCamelCaseCSS = (lines: string[]): string[] => {
+	let returnLines: string[] = [];
+	_.forEach(lines, (line) => {
+		if (!_.isEmpty(line)) {
+			let [key, value] = _.split(line, ":");
+			const newLine = `${_.camelCase(_.trim(key))}: "${_.trim(value)}",`;
+			returnLines.push(newLine);
+		}
+	});
+	return returnLines;
+};
+
+
+export function activate(context: vscode.ExtensionContext) {
+
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with registerCommand
+	// The commandId parameter must match the command field in package.json
+	let disposable = vscode.commands.registerCommand('extension.cssConverter', () => {
+		// The code you place here will be executed every time your command is executed
+		const editor = vscode.window.activeTextEditor;
+		if (!editor) {
+			vscode.window.showInformationMessage('No editor is active');
+			return;
+		}
+		const text = editor.document.getText(editor.selection);
+		let splittedText = _.split(text, ";");
+
+		let replacedText: string[] = [];
+
+		// HTML CSS to JS CSS (Generate Camel Case CSS)
+		if (splittedText.length > 1) {
+			replacedText = generateCamelCaseCSS(splittedText);
+		} else {
+			// JS CSS to HTML CSS (Generate kebab-case CSS)
+			replacedText = generateKebabCaseCSS(_.split(text, ","));
+		}
+		// replace generated css 
+		try {
+			editor.edit(e => e.replace(editor.selection, _.join(replacedText, "\n")));
+			vscode.window.showInformationMessage("Converted CSS");
+		} catch (e) {
+			vscode.window.showErrorMessage("Error while converting CSS!");
+		}
+	});
+	context.subscriptions.push(disposable);
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {}
