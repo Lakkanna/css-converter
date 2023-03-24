6d8e61dce0e6556ab36ad9975ca4975bfa65a4df
lakkanna <lakkannawalikar@gmail.com>
2023-03-24 16:33:27 +0530

    issue-21 fixed

--- src/utils/removeQuotes.ts
+++ src/utils/removeQuotes.ts
@@ -3,16 +3,16 @@
  * @description remove quotes
  */
 
- import removeWhitespaces from './removeWhitespaces';
-
- /**
-  * function to remove quotes from string
-  * @param {string} value - A value to get remove quotes
-  * @returns {string} string value removed quotes and whitespaces
-  */
- const removeQuotesFromString = (value: string): string => {
-   const withoutWhiteSpace = removeWhitespaces(value);
-   return withoutWhiteSpace.replace(/["']/g, '')
+/**
+ * function to remove quotes from string
+ * @param {string} value - A value to get remove quotes
+ * @returns {string} string value removed quotes and whitespaces
+ */
+const removeQuotesFromString = (value: string): string => {
+  // Issue: https://github.com/Lakkanna/css-converter/issues/21 and https://github.com/Lakkanna/css-converter/issues/22
+  // This is fixed now
+  const withoutWhiteSpace = value.trim();
+  return withoutWhiteSpace.replace(/["']/g, "");
 };
 
 export default removeQuotesFromString;
